ca2040462d156e321a0c6384c8ea438d2d363c02
Aparecido Celso de Souza Junior <celsojrbr@gmail.com>
2024-05-06 12:01:54 -0300

    subindo a versão 3 do desafio

--- index.js
+++ index.js
@@ -2,34 +2,37 @@
 const mensagemsaidapt1 = "O Herói de Nome ";
 const mensagemsaidapt2 = " está no nível de ";
 
-let NomeHeroi = "Aparecido Celso";
-let ExperienciaHeroi = "1001";
-let NivelDoHeroi = "";
+let NomeHeroi = ["Aparecido Celso","Felipe Aguiar","Mary Ballesta","Regiane Araujo","Iglá Generoso","Aline Antunes"];
+let ExperienciaHeroi = ["1001","5001","9001","10000","20001"];
+let NivelDoHerois = ["sem definição","sem definição","sem definição","sem definição","sem definição"];
 
-if(ExperienciaHeroi<=1000){
-    //Ferro
-    NivelDoHeroi = "Ferro";
-}else if(ExperienciaHeroi<=2000){
-    //Bronze
-    NivelDoHeroi = "Bronze";
-}else if(ExperienciaHeroi<=5000){
-    //Prata
-    NivelDoHeroi = "Prata";
-}else if(ExperienciaHeroi<=7000){
-    //Ouro
-    NivelDoHeroi = "Ouro";
-}else if(ExperienciaHeroi<=8000){
-    //Platina
-    NivelDoHeroi = "Platina";
-}else if(ExperienciaHeroi<=9000){
-    //Ascendente
-    NivelDoHeroi = "Ascendente";
-}else if(ExperienciaHeroi<=10000){
-    //Imortal
-    NivelDoHeroi = "Imortal";
-}else if(ExperienciaHeroi>=10001){
-    //Radiante
-    NivelDoHeroi = "Radiante";
-}
+for (let index = 0; index < NivelDoHerois.length; index++) {
+    if(ExperienciaHeroi[index]<=1000){
+        //Ferro
+        NivelDoHerois[index] = "Ferro";
+    }else if(ExperienciaHeroi[index]<=2000){
+        //Bronze
+        NivelDoHerois[index] = "Bronze";
+    }else if(ExperienciaHeroi[index]<=5000){
+        //Prata
+        NivelDoHerois[index] = "Prata";
+    }else if(ExperienciaHeroi[index]<=7000){
+        //Ouro
+        NivelDoHerois[index] = "Ouro";
+    }else if(ExperienciaHeroi[index]<=8000){
+        //Platina
+        NivelDoHerois[index] = "Platina";
+    }else if(ExperienciaHeroi[index]<=9000){
+        //Ascendente
+        NivelDoHerois[index] = "Ascendente";
+    }else if(ExperienciaHeroi[index]<=10000){
+        //Imortal
+        NivelDoHerois[index] = "Imortal";
+    }else if(ExperienciaHeroi[index]>=10001){
+        //Radiante
+        NivelDoHerois[index] = "Radiante";
+    }
 
-console.log(mensagemsaidapt1+NomeHeroi+mensagemsaidapt2+NivelDoHeroi);
+    console.log(mensagemsaidapt1+NomeHeroi[index]+mensagemsaidapt2+NivelDoHerois[index]);
+
+};
